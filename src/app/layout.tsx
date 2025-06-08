7ef89ecd4adbb08c02228e180912bc585392d273
Justin Gebert <justin.gebert.berlin@gmail.com>
2025-06-08 14:50:13 +0200

    fix: input font size to 16 to avoid zoom in on ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+      </head>
       <body className={inter.className}>
         <link rel="icon" href="/icons/web/favicon.ico" sizes="any" />
         <div className="p-4 h-[100dvh]">
